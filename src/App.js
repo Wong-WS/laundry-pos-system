0c4d64de84f655418d963b8f906d148215f5c7cf
Wong <66583478+Wong-WS@users.noreply.github.com>
2024-12-24 08:24:37 +0800

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import NewOrder from './components/NewOrder';
+import Pickups from './components/Pickups';
+import './App.css';
+
 function App() {
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                {/* Default Route: Redirect root to New Order */}
+                <Route path="/" element={<Navigate to="/NewOrder" />} />
+
+                {/* Specific Routes */}
+                <Route path="/NewOrder" element={<NewOrder />} />
+                <Route path="/Pickups" element={<Pickups />} />
+
+                {/* Optional: Handle unmatched routes */}
+                <Route path="*" element={<Navigate to="/NewOrder" />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
